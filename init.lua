6b480b3004b73358b1e4140616d831566f9cba6e
Arturo Fernandez <artufm at yahoo dot es>
2021-09-01 10:01:54 -0700

    Adding LSP support

--- init.lua
+++ init.lua
@@ -56,13 +56,64 @@ packer.startup(function()
   use 'editorconfig/editorconfig-vim'
   use 'Raimondi/delimitMate'
   use 'chriskempson/base16-vim'
-  use 'neovim/nvim-lspconfig'
   use {
     'hoob3rt/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons', opt = true}
   }
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'                -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp'            -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip'        -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip'                -- Snippets plugin
   end
 )
+
+o.completeopt = 'menuone,noselect'
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if vim.fn.pumvisible() == 1 then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+      elseif luasnip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if vim.fn.pumvisible() == 1 then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+      elseif luasnip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
 
 require'lualine'.setup {
   options = {
@@ -70,6 +121,7 @@ require'lualine'.setup {
   }
 }
 
+-- `gopls` requires a `go.mod` file or a `.git` directory as `root_dir`
 require'lspconfig'.gopls.setup{}
 
 map('n', '<Leader>ff',[[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]] , {noremap = true})
